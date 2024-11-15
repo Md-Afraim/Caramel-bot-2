57746f548de3e0d68dc74464db7809b5d162a8ad
Md-Afraim <alvidesu@gmail.com>
2024-11-16 04:49:16 +1100

    Update config.js

--- config.js
+++ config.js
@@ -39,9 +39,9 @@ module.exports = {
   nodes: [
     {
         name: "Server",
-        host: "lava.catfein.com",
-        port: 4000,
-        password: "catfein",
+        host: "node.lewdhutao.my.eu.org",
+        port: 80,
+        password: "youshallnotpass",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
