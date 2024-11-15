3bd17de9f82cf48cec18dd0a618021e216b5b81d
Md-Afraim <alvidesu@gmail.com>
2024-11-16 04:55:52 +1100

    Update config.js

--- config.js
+++ config.js
@@ -39,9 +39,9 @@ module.exports = {
   nodes: [
     {
         name: "Server",
-        host: "node.lewdhutao.my.eu.org",
-        port: 80,
-        password: "youshallnotpass",
+        host: "lavalink01.techbyte.host",
+        port: 2036,
+        password: "NAIGLAVA-dash.techbyte.host",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
