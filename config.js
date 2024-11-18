bad355f1d5f8fa9c6866421c49b0c134f74e6c56
Md-Afraim <alvidesu@gmail.com>
2024-11-18 23:30:50 +1100

    Update config.js

--- config.js
+++ config.js
@@ -38,10 +38,10 @@ module.exports = {
 
   nodes: [
     {
-        name: "Working2",
-        host: "lavalink.clxud.dev",
-        port: 2333,
-        password: "youshallnotpass",
+        name: "AjieDev - Lavalink [Non SSL | US]",
+        host: "lava-v3.ajieblogs.eu.org"
+        port: 80,
+        password: "https://dsc.gg/ajidevserver",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
